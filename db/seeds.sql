6ae52137f23696de68d5d239e60771a405d8f010
Da Vinci <jeb2je@gmail.com>
2019-11-12 12:59:33 -0500

    adding folders

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers (id, burger_name, devoured)
+VALUES (1, "hamburger", 0);
+
+INSERT INTO burgers (id, burger_name, devoured)
+VALUES (2, "turkey burger", 1);
+
+INSERT INTO burgers (id, burger_name, devoured)
+VALUES (3, "cheeseburger", 0);
